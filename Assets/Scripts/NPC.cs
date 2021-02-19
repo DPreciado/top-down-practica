1f0215e42636e3dab5b13aef52e8e8de478e67d7
David <arturo_preciado@live.com.mx>
2021-02-19 14:11:10 -0700

    npc con dialogo y posion consumible

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [SerializeField]
+    DialogTextBox npcTextBox;
+
+    [SerializeField, TextArea(3,5)]
+    string npcDialog;
+
+    public void startTalking()
+    {
+        npcTextBox.gameObject.SetActive(true);
+        npcTextBox.Message = npcDialog;
+        npcTextBox.ShowDialog();
+    }
+
+    public void stopTalking()
+    {
+        npcTextBox.gameObject.SetActive(false);
+        npcTextBox.ClearText();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            startTalking();
+        }    
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            stopTalking();
+        }
+    }
+}
